c40991fb727e921f82b8b2996d5f038ba4619f27
vladimir <slr-klesks-300@yandex.ru>
2022-09-25 19:18:33 +1000

    задача 1 с использованием random

---
+++ Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(minRandom, maxRandom + 1);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "\t ");
+    }
+    System.Console.WriteLine();
+}
+
+int Count(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+const int MIN_ELEMENTS = -9;
+const int MAX_ELEMENTS = 9;
+
+int[] numbers = GenerateArray(10, MIN_ELEMENTS, MAX_ELEMENTS);
+PrintArray(numbers);
+
+System.Console.WriteLine(Count(numbers));
